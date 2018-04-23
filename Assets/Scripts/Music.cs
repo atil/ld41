a428f4804e4f58e7d45fbf33bdf1886809f78b9e
atil <hyperion91@gmail.com>
2018-04-23 23:18:45 +0300

    There are no commit messages in game jams;

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AnimationCurve FadeInCurve;
+
+    public IEnumerator Start()
+    {
+        var vol = AudioSource.volume;
+        const float duration = 5f;
+        for (var f = 0f; f < duration; f += Time.deltaTime)
+        {
+            AudioSource.volume = Mathf.Lerp(0f, vol, FadeInCurve.Evaluate(f / duration));
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Ui.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ui : MonoBehaviour
+{
+    public Image BlackScreen;
+    public AnimationCurve FadeInCurve;
+
+    public IEnumerator Start()
+    {
+        const float duration = 5f;
+        for (var f = 0f; f < duration; f += Time.deltaTime)
+        {
+            var c = BlackScreen.color;
+            c.a = Mathf.Lerp(1f, 0f, FadeInCurve.Evaluate(f / duration));
+            BlackScreen.color = c;
+
+            yield return null;
+        }
+    }
+}
